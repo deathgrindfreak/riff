d9366c750df193c33c928f3e1b4c1fe3989abd59
Cooper Bell <john.cooper.bell@gmail.com>
2014-02-01 13:54:59 -0600

    Added Staff

--- riff.c
+++ riff.c
@@ -4,10 +4,42 @@
  
 #include <ncurses.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+#define X_BUFFER 2      /* x spacing for header and staff */
+
+
+void staff(int, int, int, char[]);
 
 
 main(int argc, char *argv[])
 {
+    int row, col, strings = 6;
+    char tuning[] = "EADGBE";
+
+    initscr();
+
+    getmaxyx(stdscr, row, col);
+
+    staff(strings, 2, col, tuning);
+    
+    refresh();
+    getch();
+    endwin();
 
     return 0;
+}
+
+
+void staff(int strings, int row, int max_col, char tuning[]) {
+
+    int i, j, k;
+    for (i = 0; i < strings; i++) {
+        mvprintw(row + i, X_BUFFER, "%c", tuning[strlen(tuning) - i - 1]);
+
+        for (j = 0; j < max_col - (X_BUFFER + 2); j++) {
+            mvprintw(row + i, X_BUFFER + j + 1, "%c", '-');
+        }
+    }
 }
