b9694273488b1b89188dc0d48e3baa68239b2509
Cooper Bell <john.cooper.bell@gmail.com>
2014-02-01 22:47:18 -0600

    Feeling out the interface and text placement

--- riff.c
+++ riff.c
@@ -5,25 +5,45 @@
 #include <ncurses.h>
 #include <stdlib.h>
 #include <string.h>
+//#include "big_text.h"
 
 
-#define X_BUFFER 2      /* x spacing for header and staff */
+#define HEADER_BUFFER 8    /* y buffer spacing for header */
+#define X_BUFFER 2          /* x buffer for side spacing */
+#define Y_BUFFER 2          /* y buffer for top spacing */
 
 
 void staff(int, int, int, char[]);
+void measure(int, int, int);
 
 
 main(int argc, char *argv[])
 {
-    int row, col, strings = 6;
+    int row, col, strings = 6,
+        staff_length = strings + 1;
     char tuning[] = "EADGBE";
+    char author[] = "Author";
+    char song[]   = "Song";
+    char tabbed[] = "Tabbed By: _________________________";
+    char email[]  = "Email: _________________________";
 
     initscr();
-
     getmaxyx(stdscr, row, col);
 
-    staff(strings, 2, col, tuning);
-    
+    // Header
+    mvprintw(Y_BUFFER - 1     , (col - strlen(author))/2         , "%s", author);
+    mvprintw(Y_BUFFER         , (col - strlen(song))/2           , "%s", song);
+    mvprintw(HEADER_BUFFER - 4, col - (strlen(tabbed) + X_BUFFER), "%s", tabbed);
+    mvprintw(HEADER_BUFFER - 3, col - (strlen(email) + X_BUFFER) , "%s", email);
+    mvprintw(HEADER_BUFFER - 3, X_BUFFER                         , "%s%s", "Tuning: ", tuning);
+
+    int printrow = HEADER_BUFFER;
+    while (printrow < row) {
+        staff(strings, printrow, col, tuning);
+        printrow += staff_length;
+    }
+
+
     refresh();
     getch();
     endwin();
@@ -32,14 +52,34 @@ main(int argc, char *argv[])
 }
 
 
+/* measure - inserts a measure at (y, x) == (row, col) */
+void measure(int strings, int row, int col) {
+    
+    int i;
+    for (i = 0; i < strings; i++) {
+        mvprintw(row + i, col, "%c", '|');
+    }
+}
+
+/* staff - prints out the staff with the tuning included */
 void staff(int strings, int row, int max_col, char tuning[]) {
 
     int i, j, k;
-    for (i = 0; i < strings; i++) {
-        mvprintw(row + i, X_BUFFER, "%c", tuning[strlen(tuning) - i - 1]);
-
-        for (j = 0; j < max_col - (X_BUFFER + 2); j++) {
-            mvprintw(row + i, X_BUFFER + j + 1, "%c", '-');
+    for (i = 0, j = 0; i < strings; i++) {
+        // If tuning contains a flat or a sharp
+        if (tuning[strlen(tuning) - j - 1] == '#' || tuning[strlen(tuning) - j - 1] == 'b') {
+            mvprintw(row + i, X_BUFFER - 1, "%c", tuning[strlen(tuning) - j - 2]);
+            mvprintw(row + i, X_BUFFER    , "%c", tuning[strlen(tuning) - j - 1]);
+            j += 2;
+        } else {
+            mvprintw(row + i, X_BUFFER, "%c", tuning[strlen(tuning) - j - 1]);
+            ++j;
+        }
+        mvprintw(row + i, X_BUFFER + 1, "%c", '|');
+        
+        // Print the staff
+        for (k = 0; k < max_col - (X_BUFFER + 4); k++) {
+            mvprintw(row + i, X_BUFFER + k + 2, "%c", '-');
         }
     }
 }
